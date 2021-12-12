338340349c30a9efbc19e1b7f5a0c65b9e63e36d
anangfatkur <anangfatkurrr@gmail.com>
2021-12-12 16:34:38 +0700

    getx initial bindings

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_statemixin/bindings/myBindings.dart';
 import './pages/home_page.dart';
 
 void main() {
@@ -9,11 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-   
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: HomePage(),
+      home: const HomePage(),
+      initialBinding: MyBindings(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/myController.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final myC = Get.find<MyController>();
     return Scaffold(
-      appBar: AppBar(title:const Text("Home Page"),),
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Center(
+        child: Obx(
+          () => Text(
+            "${myC.count}",
+            style: const TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
     );
   }
 }
