a31000934cbecf860f1da85630e3899b0f154eb1
anangfatkur <anangfatkurrr@gmail.com>
2021-12-12 20:05:01 +0700

    getXstorage - login getx

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_statemixin/bindings/myBindings.dart';
-import './pages/home_page.dart';
+import 'package:getx_statemixin/routes/app_page.dart';
+// import 'package:getx_statemixin/bindings/myBindings.dart';
+// import './pages/home_page.dart';
+
+//GET STORAGE
+import '../pages/home.dart';
+import '../pages/login.dart';
+import 'package:get_storage/get_storage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,8 +19,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: const HomePage(),
-      initialBinding: MyBindings(),
+      // home: const MyHomePage(),
+      home: const LoginPage(),
+      getPages: AppPage.pages,
+      // initialBinding: MyBindings(),
     );
   }
 }
